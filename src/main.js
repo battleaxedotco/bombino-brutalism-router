d0697a1a6846daddb12ae9d7a8c923a4746f0f3a
Tom Scharstein <trscharstein@gmail.com>
2020-07-02 00:05:22 -0700

    Adding Select and updated import

--- src/main.js
+++ src/main.js
@@ -27,10 +27,12 @@ import {
   PanelInfo,
   Panel,
   Row,
+  Select,
   Tabs,
   TextArea,
   Toggle,
   Wrapper,
+  WrapNode,
   FilePicker,
   ColorPicker,
 } from "brutalism";
@@ -57,10 +59,12 @@ Vue.component("Menus", Menus);
 Vue.component("Panel-Info", PanelInfo);
 Vue.component("Panel", Panel);
 Vue.component("Row", Row);
+Vue.component("Select", Select);
 Vue.component("Tabs", Tabs);
 Vue.component("TextArea", TextArea);
 Vue.component("Toggle", Toggle);
 Vue.component("Wrapper", Wrapper);
+Vue.component("wrap-node", WrapNode);
 
 new Vue({
   router,
